1941938ceab32b58190a821bf81cefa87c678596
Filipe Rodrigues <subrodrigues@gmail.com>
2017-04-18 04:30:17 +0100

    Fire Projection initial draft

---
+++ Assets/Scripts/FireProjectionInstance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectionInstance : MonoBehaviour {
+	public float projectionSpeed = 0.66f;
+	public int fireRange = 30;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate(Vector3.right * 5 * Time.deltaTime); 
+		transform.localPosition = Vector3.Lerp (transform.localPosition, 
+			new Vector3 (transform.localPosition.x + fireRange, transform.localPosition.y, transform.localPosition.z),
+			Time.deltaTime * projectionSpeed);
+	}
+}
---
+++ Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour {
+	// Instantiates a Fire Projection every 0.5 seconds
+	public float fireRate = 0.5f;
+	private float nextFire = 0.0f;
+	public GameObject mProjectile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			FireProjection ();
+		}
+	}
+
+	void FireProjection(){
+		GameObject clone = (GameObject) Instantiate (mProjectile, transform.position, Quaternion.identity);
+		Destroy (clone, 2);
+	}
+}
